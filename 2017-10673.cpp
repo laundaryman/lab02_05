49760ef8b82dce8dc10c13f7116690547ed765de
real test <clifter0122@naver.com>
2018-04-09 12:45:02 +0900

    2017-10673

---
+++ 2017-10673.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+vector<int> num;
+
+using namespace std;
+int main() {
+	int n;
+	while (cin >> n) {
+		num.push_back(n);
+	}
+	sort(num.begin(), num.end(),greater<int>());
+	cout << "ordered array" << endl;
+	for (auto i : num) {
+		cout << i << " ";
+	}
+	/*create branch*/
+}
